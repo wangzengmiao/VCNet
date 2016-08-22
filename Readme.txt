57567b089cff028836fbec63278924a48ca11f84
wangzengmiao <wangzengmiao@gmail.com>
2016-08-22 21:43:38 +0800

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,30 @@
+################################################################################################################
+# File: Readme.txt
+# Aim: A brief introduction about the usage of VCNet.
+# Author:  Zengmiao WANG
+# Email : wangzengmiao@gmail.com
+# Date  : 2016-08-16
+#---------------------------------------------------------------------------------------------------------------
+# Package required: CompQuadForm
+# function: PerformVCNet
+# Input: 
+#	Datafile------the name of the data file with ".txt" extension.
+#	Resultfile----the names of the result file with ".txt" extension.
+# Output:
+#	A ".txt" file with the name of "Resultfile".
+#---------------------------------------------------------------------------------------------------------------
+# Usage:
+#	In R environment, type the following commands:
+#	source("VCNet.R");
+#	PerformVCNet("SimulationData.txt","test_result.txt");
+# "SimulationData.txt" is the name of data file and "test_result.txt" is the name of result file.
+#---------------------------------------------------------------------------------------------------------------	
+# The format of data file and the output file:
+#	Both of the two files are text file.
+#	Input-------An example of data file (SimulationData.txt) is given in the package. In this file, the first 
+#				column is the gene ID, the second column is the exon ID, and the expression of samples are 
+#				from the third column to the last column. 
+#	Output------An example of result file (test_result.txt) is given in the package. In this file, each row 
+#				represents an possible edge. The first and second column are the two genes' IDs. The third
+#				column is the test statistics of the possible edge and the last column is the p-value.
+#---------------------------------------------------------------------------------------------------------------
---
+++ VCNet.R
@@ -0,0 +1,64 @@
+#---------------------------------------
+# Aim: perform VCNet analysis-----------
+# Author: Zengmiao WANG-----------------
+# Date: Aug 13, 2016--------------------
+# Contact: wangzengmiao@gmail.com-------
+#---------------------------------------
+# libraries to be loaded----------------
+library(CompQuadForm);  
+#--------useful function----------------
+abstract_exp = function(name1,name2,exp){
+	N = dim(exp)[2];
+	X1 = exp[which(exp[,1]==name1),3:N];
+	X2 = exp[which(exp[,1]==name2),3:N];
+	return(list(t(X1),t(X2)));
+}
+
+VCNet =	function(gene1,gene2){
+	N = dim(gene1)[1];
+	p = dim(gene1)[2];
+	q = dim(gene2)[2];
+	Cor = cor(gene1,gene2);
+	Cor1 = cor(gene1);
+	Cor2 = cor(gene2);
+	lambda1 = eigen(Cor1,only.values=T)$values;
+	lambda2 = eigen(Cor2,only.values=T)$values;
+	Lamda = rep(lambda1,each=q)*lambda2;
+	Index = which(abs(Lamda)<1e-08);
+	if(length(Index)>0){
+		Lamda[which(abs(Lamda)<1e-08)] = 0;
+	}
+	
+	TranC = 3;
+	TransCor = log((1+Cor*sqrt(TranC)/2)/(1-Cor*sqrt(TranC)/2))/sqrt(TranC);
+	TransFnorm = (N-2.5)*sum(TransCor^2);
+	results = davies(q=TransFnorm,lambda=Lamda);
+	p.value_TransFnorm = results$Qq;
+	
+	return(data.frame(TransFnorm=TransFnorm,p.value_TransFnorm=p.value_TransFnorm));
+}
+
+#---------------------------------------
+
+#Resultfile: this is the name of the output file with ".txt" extension.
+#Datafile: this is the name of the input file with ".txt" extension.
+
+PerformVCNet = function(Datafile,Resultfile){
+
+	Exp	= read.table(Datafile,header=T,sep="\t");
+	Exp[,c(1,2)] = as.character(as.matrix(Exp[,c(1,2)]));
+	N = dim(Exp)[2]-2;
+	Var = apply(Exp[,3:dim(Exp)[2]],1,var);
+	Exp = Exp[which(Var>0),]
+	gene  = unique(Exp[,1]);
+	result = data.frame();
+	
+	for(i in 1:(length(gene)-1)){
+		for(j in (i+1):length(gene)){
+			X1 = abstract_exp(gene[i],gene[j],Exp)[[1]];
+			X2 = abstract_exp(gene[i],gene[j],Exp)[[2]];
+			result = rbind(result,data.frame(gene1=gene[i],gene2=gene[j],VCNet(X1,X2)));	
+		}
+	}
+	write.table(result,file=Resultfile,sep="\t",quote=F,row.names=F,col.names=T);
+}
